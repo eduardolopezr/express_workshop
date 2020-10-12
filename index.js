83fc58bca57dbe6a0b38be29d5be4ec6a0d6f518
eduardolora7 <jesuse_166@hotmail.com>
2020-10-12 13:56:41 -0500

    Actividad 5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require("./pokedex.json");
 /*
 Los verbos HTTP
 GET 
@@ -9,9 +10,41 @@ PUT
 DELETE
 */
 app.get("/",(req, res, next)=>{
-    res.status("200");
-    res.send("Bienvenido");
+    res.status(200);
+    res.send("Bienvenido a pokedex");
+    
 });
-app.listen(3000, ()=>{
+
+app.get("/pokemon/all", (req, res, next)=>{
+    res.status(200);
+    res.send(pokemon);
+
+})
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
+    const id = req.params.id-1;
+    if (id >= 0 && id <= 151){
+        res.status(200);
+        res.send(pokemon[req.params.id-1]);
+    }else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+
+
+});
+app.get('/pokemon/:name', (req, res, next)=>{
+    const name = req.params.name;
+
+    for(i=0; i<pokemon.length; i++){
+        if(pokemon[i].name==name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
 });
