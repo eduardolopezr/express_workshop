7efdbe6b8d3d64bfc8b729f9f1271ceec7f10006
eduardolora7 <jesuse_166@hotmail.com>
2020-10-21 22:15:48 -0500

    Middleware para 404 y cambio de bodyparser

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const bodyParser = require('body-parser');
 const morgan =  require("morgan");
-const { response } = require('express');
 const express = require('express');
 const app = express();
 const pokemon=require('./routes/pokemon');
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 /*
 Los verbos HTTP
 GET -> Obtiene recursos
@@ -17,11 +16,14 @@ PATCH -> Modifica una parte de recurso
 DELETE -> Borra un recurso
 */
 app.get("/",(req, res, next)=>{
-    return res.status(200).send("Bienvenido a pokedex");
+    return res.status(200).json({code:1, message: "Bienvenido al pokedex"});
 });
 
 app.use("/pokemon", pokemon); 
+app.use((req, res, next)=>{
+    return res.status(404).json({code: 404, message:"URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
-});
+}); 
